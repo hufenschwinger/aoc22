585aabe14372ac0022b20dd808b77cda6c764074
Mark Wiese <mark.wiese@stud.hs-ruhrwest.de>
2022-11-26 12:07:15 +0100

    use unique-pointer

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
+#include <memory>
 #include "Days/IDay.h"
 #include "Days/DayFourtyTwo.h"
 
-void execute(const aoc22::IDay *iday) { //TODO maybe supplier as param?
-    //TODO measure
-    const uint8_t number = iday->number();
-    const std::string solutionOne = iday->partOne();
-    const std::string solutionTwo = iday->partTwo();
+void execute(std::unique_ptr<aoc22::IDay> iday) {
+    //TODO measure executions
+    const auto& number = iday->number();
+    const auto& solutionOne = iday->partOne();
+    const auto& solutionTwo = iday->partTwo();
     std::cout << "Day: " << number << std::endl
               << "Part 1: " << solutionOne << std::endl
               << "Part 2: " << solutionTwo << std::endl;
 }
 
 int main() {
-    aoc22::IDay day;
-    execute(&day);
+    execute(std::make_unique<aoc22::DayFourtyTwo>());
     return 0;
 }
