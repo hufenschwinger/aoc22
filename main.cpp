ca88d2d1766ae21aebeb28f8c2a05da97efd2b0f
Mark Wiese <mark.wiese@stud.hs-ruhrwest.de>
2022-11-26 11:07:01 +0100

    migrate to supplier

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <functional>
 #include "Days/IDay.h"
 #include "Days/DayFourtyTwo.h"
 
-void execute(const aoc22::IDay *iday) { //TODO maybe supplier as param?
-    //TODO measure
+void execute(const std::function<aoc22::IDay*(void)>& supplier) {
+    //TODO measure executions
+    const aoc22::IDay *iday = supplier();
     const uint8_t number = iday->number();
     const std::string solutionOne = iday->partOne();
     const std::string solutionTwo = iday->partTwo();
     std::cout << "Day: " << number << std::endl
               << "Part 1: " << solutionOne << std::endl
               << "Part 2: " << solutionTwo << std::endl;
+    delete iday;
 }
 
 int main() {
-    aoc22::IDay *day = new aoc22::DayFourtyTwo();
-    execute(day);
-    delete day;
+    execute([]() -> aoc22::DayFourtyTwo* {return new aoc22::DayFourtyTwo();});
     return 0;
 }
